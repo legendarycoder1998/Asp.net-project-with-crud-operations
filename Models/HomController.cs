e01f408a4f90500d66aaab4b4e0cb0f67bbdb591
legendarycoder1998 <38974739+legendarycoder1998@users.noreply.github.com>
2023-03-15 22:11:08 +0530

    Add files via upload

---
+++ Models/HomController.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Web.Mvc;
+
+namespace CRUD_project.Models
+{
+    public class HomController : Controller
+    {
+        MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;password=1998;database=category");
+        // GET: Hom
+
+        public ActionResult Index()
+        {
+            MySqlDataAdapter adp = new MySqlDataAdapter("SELECT * FROM product_list",conn);
+
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            return View(dt);
+        }
+    }
+}
